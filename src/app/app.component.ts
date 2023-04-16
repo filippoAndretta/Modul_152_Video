93465e0021e467db287aea6d02ac8fa2dacf1ba4
Filippo Andretta <filippo.andretta@bluewin.ch>
2023-04-16 16:28:06 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {AngularEmbedVideoService} from "angular-embed-video";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Modul_152_Video';
+
+  youtubeUrl = 'https://www.youtube.com/watch?v=BM1J_cktJAw';
+
+  youtubeId = 'BM1J_cktJAw';
+
+  constructor(private embedService: AngularEmbedVideoService) {
+    console.log(this.embedService.embed(this.youtubeUrl));
+
+    console.log(this.embedService.embed_youtube(this.youtubeId));
+  }
 }
